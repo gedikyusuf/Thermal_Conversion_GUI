82963e4a3fdb18c58ddae43158c9975571d5d036
gedikyusuf <gediky21@itu.edu.tr>
2025-11-18 12:33:01 +0300

    Create __init__.py

---
+++ thermal_converter/__init__.py
@@ -0,0 +1,6 @@
+# thermal_converter/__init__.py
+"""
+ThermalConverter: A GUI utility for batch conversion of thermal RJPG files
+to radiometric TIFFs using DJI SDK, ImageJ, and ExifTool.
+"""
+__version__ = "1.0.0"
